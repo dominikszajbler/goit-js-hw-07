f8ecd406ae5706ee5d4db3c4e33cfee29a762a24
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-26 21:57:35 +0100

    modal problem

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,10 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-const galleryItem = galleryItems.map(
-    (item) =>
-    `<div class="gallery__item">
+const galleryItem = galleryItems
+	.map(
+		(item) =>
+			`<div class="gallery__item">
     <a class="gallery__link" href="${item.original}">
       <img
         class="gallery__image"
@@ -13,20 +14,24 @@ const galleryItem = galleryItems.map(
         alt="${item.description}"
       />
     </a>
-  </div>`
-).join("");
+  </div>`,
+	)
+	.join("");
 gallery.innerHTML = galleryItem;
-// import * as basicLightbox from 'basiclightbox'  
-gallery.addEventListener("click",(event) => {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") return;
-    // basicLightbox.create(`
-    //     <img width="1400" height="900" src="${item.original}">
-    // `)
-    // .show()
+gallery.addEventListener("click", (event) => {
+	event.preventDefault();
+	if (event.target.nodeName !== "IMG") return;
+    document.querySelector('.gallery__image').onclick = () => {
 
+        basicLightbox.create(`
+            <img width="1400" height="900" src="${event.target.dataset.source}">
+        `).show()
+    }
+    // }
+	// basicLightbox.create(`
+	//     <img width="1400" height="900" src="${event.target.dataset.source}">
+	// `).show()
 });
-
 
 // import * as basicLightbox from 'basiclightbox'
 
