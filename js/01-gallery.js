2605c0fc9025ffa5659a06b48a0608a5317bfa8d
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-26 21:36:04 +0100

    task1 in progress

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryItem = galleryItems.map(
+    (item) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`
+).join("");
+gallery.innerHTML = galleryItem;
+// import * as basicLightbox from 'basiclightbox'  
+gallery.addEventListener("click",(event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+    // basicLightbox.create(`
+    //     <img width="1400" height="900" src="${item.original}">
+    // `)
+    // .show()
+
+});
+
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()
